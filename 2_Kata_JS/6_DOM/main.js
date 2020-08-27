65f63eaece62c380ee6280adf32525d460117ff6
maui <maui@devf.mx>
2020-08-26 20:43:02 -0500

    Agregar 6_DOM

---
+++ 2_Kata_JS/6_DOM/main.js
@@ -0,0 +1,16 @@
+/*
+  index.html
+*/
+// var enlace = document.getElementById("link");
+// enlace.href = "https://www.google.com";
+
+// var parrafos = document.getElementsByTagName("p");
+
+/*
+  eventos.html
+*/
+
+function evolucionarPollitos() {
+  var imagenPollito = document.getElementById("imagen");
+  imagenPollito.src = "https://cdn.bulbagarden.net/upload/thumb/2/29/256Combusken.png/1200px-256Combusken.png";
+}
