ad7682ff1135b7eff981e26b5bf9c313e4231915
Malinali Becerril (malibb98) <mali@devf.mx>
2020-09-22 20:03:57 -0500

    creación de lista enlazada vacia

---
+++ 3_Kata_Computer_Science/5_Estructuras_De_Datos/linked_list.js
@@ -0,0 +1,18 @@
+// Linked list
+
+// Una lista ligada debe de tener una cabecera (head)
+// Nos va a indicar el inicio de la lista.
+
+// El tamaño de la lista (length)
+
+const lista = {
+    head: 'loquesea',
+    length: 0
+}
+
+class LinkedList{
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+}
