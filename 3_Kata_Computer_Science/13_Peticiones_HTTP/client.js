ce0d66f34fbab2c0c6e4844e7d87fd22756fb132
maui <maui@devf.mx>
2020-11-19 21:44:30 -0600

    Agregar solucion de peticion a la API de la NASA

--- 3_Kata_Computer_Science/13_Peticiones_HTTP/client.js
+++ 3_Kata_Computer_Science/13_Peticiones_HTTP/client.js
@@ -53,7 +53,7 @@ const getAuthorsByBookName = (bookName) => {
   })
 }
 
-getAuthorsByBookName("i robot");
+// getAuthorsByBookName("i robot");
 
 /*
 3.- Hacer una petición por autor y devolver la lista de 
@@ -70,7 +70,37 @@ getAuthorsByBookName("i robot");
 6.- Devolver los asteroides que sean potencialmente peligrosos
     para la tierra de la semana pasada hasta el día de ayer.
                     https://api.nasa.gov/
+*/
+                          
+const getGlobalExtinction = (startDate, endDate) => {
+  const API_KEY = 'D6O9rBSNALehqQBmm9S5aQEIwDnI5Q6f9Ws5HyVB';
+  const NASA_URL = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${API_KEY}`;
 
+  request.get(NASA_URL, (err, res, body) => {
+    if (res.statusCode === 200) {
+      const json = JSON.parse(body);
+      // json.near_earth_objects.2015-09-08[n].is_potentially_hazardous_asteroid (pueden ser muchas fechas)
+      const dates = Object.keys(json.near_earth_objects);
+      dates.forEach(date => {
+        console.log('\n', date);
+        json.near_earth_objects[date]
+          .forEach(asteroid => {
+            const name = asteroid.name;
+            const magnitude = asteroid.absolute_magnitude_h;
+            const isHazardous = asteroid.is_potentially_hazardous_asteroid;
+
+            if (isHazardous) {
+              console.log(`${name} with magnitude ${magnitude} is hazardous? ${isHazardous}`);
+            }
+          });
+      }); 
+    }
+  });
+}
+
+getGlobalExtinction("2020-11-01", "2020-11-08");
+
+/*
 7.- Traer los primeros 151 pokemon de la primera generacion y 
     devolver un arreglo de objetos con el nombre, sus moves, tipos, tamaño 
     y peso.
