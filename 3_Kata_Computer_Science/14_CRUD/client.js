f878c8f86a85231b5d44b5bebb189316fa2172b1
maui <maui@devf.mx>
2020-11-23 22:10:47 -0600

    Agregar CRUD de Autores

--- 3_Kata_Computer_Science/14_CRUD/client.js
+++ 3_Kata_Computer_Science/14_CRUD/client.js
@@ -2,19 +2,46 @@ const request = require('request');
 
 const URL_BASE = 'https://goodreads-devf-aaron.herokuapp.com';
 
-const createAuthor = () => {
+/*
+  CRUD Peticiones:
+
+  - CREATE
+  - READ
+  - UPDATE
+  - DELETE
+*/
+
+// CREATE
+const createAuthor = (name, lastName, nacionalidad, biography, gender, age, isAlive) => {
   const URL_POST = `${URL_BASE}/api/v1/authors/`;
   const jsonSend = {
-    "name": "Maui",
-    "last_name": "Generacion 2",
-    "nacionalidad": "MX",
-    "biography": "Sensei jeje",
-    "gender": "M",
-    "age": 25
+    "name": name,
+    "last_name": lastName,
+    "nacionalidad": nacionalidad,
+    "biography": biography,
+    "gender": gender,
+    "age": age,
+    "is_alive": isAlive
   };
 
-  request.post(URL_POST, { form: jsonSend }, (err, res, body) => {
+  return request.post(URL_POST, { form: jsonSend }, (err, res, body) => {
     if (res.statusCode === 201) {
+      const newAuthor = JSON.parse(body);
+      console.log(newAuthor);
+      return newAuthor;
+    } else {
+      console.log(res.statusCode);
+    }
+  });
+}
+
+// createAuthor("José", "Saramago", "USA", "Escritor de muchos libros", "M", 95, false);
+
+// READ ALL
+const findAllAuthors = () => {
+  const URL_GET = `${URL_BASE}/api/v1/authors/`;
+  request.get(URL_GET, (err, res, body) => {
+    if (res.statusCode === 200) {
       const json = JSON.parse(body);
       console.log(json);
     } else {
@@ -23,4 +50,65 @@ const createAuthor = () => {
   });
 }
 
-createAuthor();
+// findAllAuthors();
+
+// READ ONE
+const findOneAuthorById = (id) => {
+  const URL_GET_ONE = `${URL_BASE}/api/v1/authors/${id}/`;
+  request.get(URL_GET_ONE, (err, res, body) => {
+    if (res.statusCode === 200) {
+      const json = JSON.parse(body);
+      console.log(json);
+    } else {
+      console.log(res.statusCode);
+    }
+  });
+}
+
+// findOneAuthorById(12818);
+
+// Este es un típico error ocasionado por la asincronicidad de las peticiones HTTP mediante AJAX
+// const newAuthor = createAuthor("José", "Saramago", "USA", "Escritor de muchos libros", "M", 95, false);
+// findOneAuthorById(newAuthor.id);
+
+// UPDATE ONE
+const updateAuthor = (id, name, lastName, nacionalidad, biography, gender, age, isAlive) => {
+  const URL_PUT = `${URL_BASE}/api/v1/authors/${id}/`;
+  const jsonSend = {
+    "name": name,
+    "last_name": lastName,
+    "nacionalidad": nacionalidad,
+    "biography": biography,
+    "gender": gender,
+    "age": age,
+    "is_alive": isAlive
+  };
+
+  return request.put(URL_PUT, { form: jsonSend }, (err, res, body) => {
+    if (res.statusCode === 200) {
+      const updatedAuthor = JSON.parse(body);
+      console.log(updatedAuthor);
+      return updatedAuthor;
+    } else {
+      console.log(res.statusCode);
+    }
+  });
+}
+
+// updateAuthor(12818, "Elías", "Saramago", "USA", "Escritor de mucho código", "F", 371, true);
+
+// DELETE ONE
+const deleteAuthorById = (id) => {
+  const URL_DELETE_ONE = `${URL_BASE}/api/v1/authors/${id}/`;
+  request.delete(URL_DELETE_ONE, (err, res, body) => {
+    if (res.statusCode === 204) {
+      console.log(`Usuario con id ${id} fue eliminado`);
+    } else {
+      console.log(res.statusCode);
+    }
+  });
+}
+
+// createAuthor("José", "Saramago", "USA", "Escritor de muchos libros", "M", 95, false);
+// deleteAuthorById(13257);
+// findOneAuthorById(13257);
