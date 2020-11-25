406ee7ac234ceb116e3e6635cb491457a3dd246b
maui <maui@devf.mx>
2020-11-24 21:10:34 -0600

    Agregar ejemplo de peticion con promesa

--- 3_Kata_Computer_Science/15_Promesas/client.js
+++ 3_Kata_Computer_Science/15_Promesas/client.js
@@ -2,29 +2,53 @@ const request = require("request");
 
 const AoE_API = 'https://age-of-empires-2-api.herokuapp.com';
 
-const getAllCivilizations = () => {
-  const CIVILIZATIONS = '/api/v1/civilizations';
-  const GET_CIVILIZATIONS = `${AoE_API}${CIVILIZATIONS}`;
+// const getAllCivilizations = () => {
+//   const CIVILIZATIONS = '/api/v1/civilizations';
+//   const GET_CIVILIZATIONS = `${AoE_API}${CIVILIZATIONS}`;
 
-  request.get(GET_CIVILIZATIONS, (err, res, body) => {
-    if (res.statusCode === 200) {
-      const json = JSON.parse(body);
-      getOneCivilization(json);
-    }
-  });
-}
+//   request.get(GET_CIVILIZATIONS, (err, res, body) => {
+//     if (res.statusCode === 200) {
+//       const json = JSON.parse(body);
+//       getOneCivilization(json);
+//     }
+//   });
+// }
 
-const getOneCivilization = (json) => {
-  const civilizationName = json.civilizations[3].name;
-  const ONE_CIVILIZATION = `/api/v1/civilization/${civilizationName}`
-  const GET_ONE_CIVILIZATION = `${AoE_API}${ONE_CIVILIZATION}`;
+// const getOneCivilization = (json) => {
+//   const civilizationName = json.civilizations[3].name;
+//   const ONE_CIVILIZATION = `/api/v1/civilization/${civilizationName}`
+//   const GET_ONE_CIVILIZATION = `${AoE_API}${ONE_CIVILIZATION}`;
 
-  request.get(GET_ONE_CIVILIZATION, (err, res, body) => {
-    if (res.statusCode == 200) {
-      console.log(JSON.parse(body));
-    }
-  })
-}
+//   request.get(GET_ONE_CIVILIZATION, (err, res, body) => {
+//     if (res.statusCode == 200) {
+//       console.log(JSON.parse(body));
+//     }
+//   })
+// }
 
 // getAllCivilizations();
 
+const getAllCivilizations = () => {
+  const GET_CIVILIZATIONS = `${AoE_API}/api/v1/civilizations`;
+
+  return new Promise((resolve, reject) => {
+    request.get(GET_CIVILIZATIONS, (err, res, body) => {
+      if (res.statusCode === 200) {
+        const json = JSON.parse(body);
+        resolve(json);
+      }
+      reject(new Error("No se completó la petición..."));
+    });
+  })
+}
+
+// const promesa = getAllCivilizations();
+
+// promesa
+//   .then(exito => console.log(exito))
+//   .catch(error => console.log(error));
+
+
+getAllCivilizations()
+  .then(civilizations => console.log(civilizations))
+  .catch(error => console.log(error));
