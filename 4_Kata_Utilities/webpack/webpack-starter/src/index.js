078631e8a7c654ba058e8a0949b3a3080e63f0d8
Malinali Becerril <mali@devf.mx>
2020-11-30 18:32:58 -0600

    fixs

--- 4_Kata_Utilities/webpack/webpack-starter/src/index.js
+++ 4_Kata_Utilities/webpack/webpack-starter/src/index.js
@@ -2,4 +2,6 @@
 // const greeting = require('./greeting');
 // BAbel o ECMA2015 ecma6 la forma que corre node
 import greeting from './greeting';
+
+// usar la función
 console.log(greeting('generación 2'));
