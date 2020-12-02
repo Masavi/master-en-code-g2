abb3f3bcb3dac66f3d7bd7723675b13b26f56766
Malinali Becerril <mali@devf.mx>
2020-12-01 22:03:36 -0600

    add axios peticion

---
+++ 4_Kata_Utilities/webpack/webpack-pokedex/src/pokedex.js
@@ -0,0 +1,14 @@
+// Almacenar el arreglo de pokemones
+class Pokedex{
+    constructor( pokemones = [] ){
+        this.pokemones = pokemones;
+    }
+    renderPokemons(){
+
+    }
+    searchPokemons(){
+        
+    }
+}
+
+export default Pokedex;
