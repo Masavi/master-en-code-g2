708d5e78d55340b3f0bc053622895a03e702f79d
maui <maui@devf.mx>
2021-02-02 21:35:46 -0600

    Agregar instrucciones del ejercicio

--- 5_Kata_Frontend/todo-app/src/App.js
+++ 5_Kata_Frontend/todo-app/src/App.js
@@ -11,6 +11,22 @@ function App() {
       </header>
     </div>
   );
+
+  /*
+  // Ejercicio:
+
+  return (
+    <main className="main">
+      <Navbar /> - Colocar una navbar sencilla
+      <Content /> 
+        - Una lista de contactos (pueden colocar los nombres y correos de tres personas)
+        - Tiene que ocupar todo el espacio disponible entre el navbar y el footer
+        - Tiene que centrar el contenido horizontal y verticalmente
+      <Footer /> - Colocar un footer sencillo que siempre esté pegado al final de la página, independientemente del contenido
+    </main>
+  )
+
+  */
 }
 
 export default App;
