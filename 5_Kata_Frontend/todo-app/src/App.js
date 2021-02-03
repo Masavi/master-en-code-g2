858cd58aa3249d7bc6982ab7957baac49b13507a
maui <maui@devf.mx>
2021-02-02 20:36:59 -0600

    Repaso de componentes de clase

---
+++ 5_Kata_Frontend/todo-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import HolaMundo from './components/HolaMundo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Todo App
+        <HolaMundo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
