5256cac054c8d015d5ec7647f64c72196616ca13
maui <maui@devf.mx>
2021-02-09 21:08:23 -0600

    Agregar ejemplo de rutas con links desde navbar

--- 5_Kata_Frontend/react-router/src/App.js
+++ 5_Kata_Frontend/react-router/src/App.js
@@ -11,10 +11,22 @@ export default function App() {
   return (
     <Router>
       <nav className="navbar">
-        <Link className="navbar__link">Home</Link>
-        <Link className="navbar__link">Ejemplo</Link>
-        <Link className="navbar__link">Peticiones</Link>
+        <Link to="/" className="navbar__link">Home</Link>
+        <Link to="/ejemplo" className="navbar__link">Ejemplo</Link>
+        <Link to="/peticiones" className="navbar__link">Peticiones</Link>
       </nav>
+      <Switch>
+        <Route exact path="/">
+          <h1>Inicio</h1>
+        </Route>
+        <Route exact path="/ejemplo">
+          <h1>Ejemplo</h1>
+        </Route>
+        <Route exact path="/peticiones">
+          <h1>Peticiones</h1>
+          <h2>useEffect</h2>
+        </Route>
+      </Switch>
     </Router>
   );
 }
