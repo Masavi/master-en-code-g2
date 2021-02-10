7740d94d0aed0449427531dfa71393a76abbd7f2
maui <maui@devf.mx>
2021-02-09 20:44:05 -0600

    Agregar navbar y estilos

--- 5_Kata_Frontend/react-router/src/App.js
+++ 5_Kata_Frontend/react-router/src/App.js
@@ -5,64 +5,16 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import './App.css';
 
 export default function App() {
   return (
-    /* 
-      Router siempre debe ir al inicio (o hasta arriba)
-      de nuestra aplicación para poder manejar este
-      enrutador
-    */
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              {/*
-                Link nos permite cambiar la ruta
-                DENTRO de nuestra app. Tiene que estar
-                adentro de "Router" para funcionar
-              */}
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/*
-          El switch se encarga de "cambiar/switchear"
-          los componentes dependiendo de la ruta actual
-          de tu aplicación
-        */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <nav className="navbar">
+        <Link className="navbar__link">Home</Link>
+        <Link className="navbar__link">Ejemplo</Link>
+        <Link className="navbar__link">Peticiones</Link>
+      </nav>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
