f62e25e19f3abb0691c095e349a55385856094b0
maui <maui@devf.mx>
2021-02-15 19:38:52 -0600

    Re-estructurar archivos para reto de TVMaze API

--- 5_Kata_Frontend/react-router/src/App.js
+++ 5_Kata_Frontend/react-router/src/App.js
@@ -6,22 +6,14 @@ import {
 } from "react-router-dom";
 import Routes from './Routes';
 import './App.scss';
-import NavbarComponent from './components/NavbarComponent';
 
 export default function App() {
-  // const id = 7;
-
   return (
     <Router>
-      <NavbarComponent />
-      {/* <nav className="navbar">
-        <Link to="/" className="navbar__link">Home</Link>
-        <Link to="/ejemplo" className="navbar__link">Ejemplo</Link>
-        <Link to={`/autor/${id}`} className="navbar__link">Autor</Link>
-        <Link to="/peticiones" className="navbar__link">Peticiones</Link>
-      </nav> */}
       <Switch>
-        <Routes />
+        <main className="container mt-4">
+          <Routes />
+        </main>
       </Switch>
     </Router>
   );
--- 5_Kata_Frontend/react-router/src/Routes.js
+++ 5_Kata_Frontend/react-router/src/Routes.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Peticiones from './components/Peticiones';
-import Autor from './components/Autor';
+// import Peticiones from './components/Peticiones';
+// import Autor from './components/Autor';
 
 function Routes() {
   return (
     <>
       <Route exact path="/">
-        <div className="container pt-4">
-          <h1>Inicio</h1>
-        </div>
-      </Route>
-      <Route exact path="/ejemplo">
-        <h1>Ejemplo</h1>
-      </Route>
-      <Route exact path="/autor/:idAutor">
-        <Autor />
-      </Route>
-      <Route exact path="/peticiones">
-        <Peticiones />
+        <h1>Inicio</h1>
       </Route>
     </>
   )
