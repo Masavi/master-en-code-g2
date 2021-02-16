d18bbf800a81f13d84f486cb08fe5aed937b3139
maui <maui@devf.mx>
2021-02-15 21:29:23 -0600

    Mostrar data de shows en Home al hacer search

--- 5_Kata_Frontend/react-router/src/components/Home.js
+++ 5_Kata_Frontend/react-router/src/components/Home.js
@@ -3,8 +3,26 @@ import PropTypes from 'prop-types';
 
 const Home = ({ shows }) => {
   console.log('En Home:', shows);
-  return ( 
-    <h1>Home</h1>
+  // shows.show ->
+  // show.name
+  // show.image.medium
+  // show.summary
+
+  const showResults = () => {
+    return shows.map(showObject => {
+      // eslint-disable-next-line react/jsx-key
+      return (<h3>{ showObject.show.name }</h3>)
+    });
+  };
+
+  return (
+    <>
+      {
+        shows.length > 0
+          ? showResults()
+          : <h1>Home</h1>
+      }
+    </>
   );
 }
 
