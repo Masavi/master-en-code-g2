427199314b81151ec4d255ef5c2a1ab8014917ba
Malinali Becerril <mali@devf.mx>
2021-02-18 19:10:27 -0600

    useForm

--- 5_Kata_Frontend/quoteapp/src/views/Login.js
+++ 5_Kata_Frontend/quoteapp/src/views/Login.js
@@ -1,25 +1,44 @@
 import React from 'react';
+import useForm from '../hooks/useForm';
+// si voy a cambiar la data mientras se encuentra montado el componente
+// eso sisgnifica que voy a utilizar el estado
 
 export default function Login() {
+    const sendForm = (inputs) => {
+        console.log('Ejecuté send form', inputs);
+    };
+
+    const {
+        inputs,
+        handleInputs,
+        handleSubmit,
+    } = useForm(sendForm,
+        {
+        email:"mali",
+        password:"gatitos59"
+    });
+
     return(
-        <div className="container my-4">
-            <div className="row">
-            <div  className="col-7 mt-3">
-                <div className="input-group flex-nowrap">
-                <span className="input-group-text" id="addon-wrapping">@</span>
-                <input type="text" className="form-control" id="email" placeholder="Email" aria-label="email" aria-describedby="addon-wrapping"/>
+        <form onSubmit={handleSubmit}>
+            <div className="container my-4">
+                <div className="row">
+                <div  className="col-7 mt-3">
+                    <div className="input-group flex-nowrap">
+                    <span className="input-group-text" id="addon-wrapping">@</span>
+                    <input type="text" value={inputs.email} onChange={handleInputs} className="form-control" id="email" placeholder="Email" aria-label="email" aria-describedby="addon-wrapping"/>
+                    </div>
+                </div>
+                <div  className="col-7 mt-3">
+                    <div className="input-group flex-nowrap">
+                    <span className="input-group-text" id="addon-wrapping">@</span>
+                    <input type="password" value={inputs.password} onChange={handleInputs} className="form-control" id="password" placeholder="Contraseña" aria-label="password" aria-describedby="addon-wrapping"/>
+                    </div>
+                </div>
+                <div className="col-6 mt-4">
+                    <button type="submit" className="btn btn-info">Iniciar sesión</button>
+                </div>
                 </div>
             </div>
-            <div  className="col-7 mt-3">
-                <div className="input-group flex-nowrap">
-                <span className="input-group-text" id="addon-wrapping">@</span>
-                <input type="password" className="form-control" id="password" placeholder="Contraseña" aria-label="password" aria-describedby="addon-wrapping"/>
-                </div>
-            </div>
-            <div className="col-6 mt-4">
-                <button type="button" class="btn btn-info">Iniciar sesión</button>
-            </div>
-            </div>
-        </div>
+        </form>
     );
 };
--- 5_Kata_Frontend/quoteapp/src/views/Signup.js
+++ 5_Kata_Frontend/quoteapp/src/views/Signup.js
@@ -1,43 +1,56 @@
 import React from 'react';
+import useForm from '../hooks/useForm';
 
 export default function Signup() {
+    const sendForm2Elregresodelosformsasesinos = (inputs) => {
+        console.log('Ejecuté sendForm2Elregresodelosformsasesino', inputs);
+    };
+    
+    const {
+        inputs,
+        handleInputs,
+        handleSubmit,
+    } = useForm(sendForm2Elregresodelosformsasesinos, {});
+
     return(
-        <div className="container my-4">
+        <form onSubmit={handleSubmit}>
+            <div className="container my-4">
             <div className="row">
             <div  className="col-7 mt-3">
                 <div className="input-group flex-nowrap">
                 <span className="input-group-text" id="addon-wrapping">@</span>
-                <input type="text" className="form-control" id="email" placeholder="Email" aria-label="email" aria-describedby="addon-wrapping"/>
+                <input type="text" value={inputs.email} onChange={handleInputs} className="form-control" id="email" placeholder="Email" aria-label="email" aria-describedby="addon-wrapping"/>
                 </div>
             </div>
             <div  className="col-7 mt-3">
                 <div className="input-group flex-nowrap">
                 <span className="input-group-text" id="addon-wrapping">@</span>
-                <input type="text" className="form-control" id="first_name" placeholder="Primer Nombre" aria-label="first_name" aria-describedby="addon-wrapping"/>
+                <input type="text" value={inputs.first_name} onChange={handleInputs} className="form-control" id="first_name" placeholder="Primer Nombre" aria-label="first_name" aria-describedby="addon-wrapping"/>
                 </div>
             </div>
             <div  className="col-7 mt-3">
                 <div className="input-group flex-nowrap">
                 <span className="input-group-text" id="addon-wrapping">@</span>
-                <input type="text" className="form-control" id="last_name" placeholder="Apellido" aria-label="last_name" aria-describedby="addon-wrapping"/>
+                <input type="text" value={inputs.last_name} onChange={handleInputs} className="form-control" id="last_name" placeholder="Apellido" aria-label="last_name" aria-describedby="addon-wrapping"/>
                 </div>
             </div>
             <div  className="col-7 mt-3">
                 <div className="input-group flex-nowrap">
                 <span className="input-group-text" id="addon-wrapping">@</span>
-                <input type="password" className="form-control" id="password" placeholder="Contraseña" aria-label="password" aria-describedby="addon-wrapping"/>
+                <input type="password" value={inputs.password} onChange={handleInputs} className="form-control" id="password" placeholder="Contraseña" aria-label="password" aria-describedby="addon-wrapping"/>
                 </div>
             </div>
             <div  className="col-7 mt-3">
                 <div className="input-group flex-nowrap">
                 <span className="input-group-text" id="addon-wrapping">@</span>
-                <input type="text" className="form-control" id="password-confirmation" placeholder="Confirmación de contraseña" aria-label="password-confirmation" aria-describedby="addon-wrapping"/>
+                <input type="text" value={inputs.password_confirmation} onChange={handleInputs} className="form-control" id="password_confirmation" placeholder="Confirmación de contraseña" aria-label="password_confirmation" aria-describedby="addon-wrapping"/>
                 </div>
             </div>
             <div className="col-6 mt-4">
-                <button type="button" class="btn btn-info">Crear Cuenta</button>
+                <button type="submit" class="btn btn-info">Crear Cuenta</button>
             </div>
             </div>
         </div>
+        </form>
     );
 };
