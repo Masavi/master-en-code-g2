6e95892fd1bab0a877a8c755542668f2a8cb210a
maui <maui@devf.mx>
2021-03-09 20:31:17 -0600

    Agregar ejemplo simple de light theme vs dark theme (Cambiar colores de app

--- 5_Kata_Frontend/context-api/src/App.js
+++ 5_Kata_Frontend/context-api/src/App.js
@@ -1,12 +1,53 @@
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
+import { useState } from "react";
+
+// import ClickCounter from './components/ClickCounter';
+// import HoverCounter from './components/HoverCounter';
 
 function App() {
+
+  const lightTheme = {
+    mode: 'light',
+    backgroundColor: 'white',
+    color: 'black',
+  }
+
+  const darkTheme = {
+    mode: 'dark',
+    backgroundColor: 'black',
+    color: 'white',
+  }
+
+  const [theme, setTheme] = useState(lightTheme);
+
+  const toggleTheme = () => {
+    if (theme.mode === 'light') {
+      return setTheme(darkTheme);
+    }
+    return setTheme(lightTheme);
+  }
+
   return (
     <div>
-      <h1>Higher Order Components</h1>
-      <ClickCounter />
-      <HoverCounter />
+      <h1>Context API</h1>
+      <article style={{
+        backgroundColor: theme.backgroundColor,
+        width: '500px',
+        height: '300px',
+        display: 'flex',
+        margin: '1.5em',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <h2 style={{
+          color: theme.color,
+        }}>Context App</h2>
+        <button
+          onClick={toggleTheme}
+          type="button"> 
+          Change Theme
+        </button>
+      </article>
     </div>
   );
 }
