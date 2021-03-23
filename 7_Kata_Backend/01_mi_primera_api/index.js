acc51af4ff1d3b0376f04fb7a67be4f67079d170
maui <maui@devf.mx>
2021-03-22 20:36:09 -0600

    Agregar ejemplo de endpoint en raiz

--- 7_Kata_Backend/01_mi_primera_api/index.js
+++ 7_Kata_Backend/01_mi_primera_api/index.js
@@ -9,9 +9,31 @@ const app = express();
 // También es común manejar el puerto del servidor desde una constante
 const PORT = 3000;
 
+/*
+  Configuración de nuestro primer endpoint
+
+  request -> contiene la información de petición de un cliente
+  response -> contiene todo lo necesario para responderle a un cliente
+*/
+app.get('/', (request, response) => {
+  response.send('¡Hola Mundo!');
+});
+
+/* 
+  Esta es la dirección local de nuestra computadora:
+  127.0.0.1 -> localhost
+
+  Esta es la dirección local de nuestro backend (asumiendo que está corriendo en el puerto 3000)
+  127.0.0.1:3000
+  localhost:3000
+*/
+
 /* 
   Para que nuestro servidor empiece a escuchar peticiones, debemos ejecutar
   el método listen. Al ejecutar este método, express toma control total de 
   la terminal desde donde fue ejecutado el archivo "index.js"
+
+  Todas las configuraciones de nuestra app de express deben ir antes de este método
+  ESTE MÉTODO SIEMPRE VA HASTA EL FINAL
 */
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
