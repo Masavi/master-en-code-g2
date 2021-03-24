11d2e90bf48845165e6b2716f347a907c6071ebb
maui <maui@devf.mx>
2021-03-23 20:20:14 -0600

    Agregar solucion ejercicio 1

---
+++ 7_Kata_Backend/01_mi_primera_api/ejercicios.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+/*
+  1.- Agrega un endpoint ‘/api/’ que responda a 
+  una petición de tipo GET con un código de estado 200 
+  y el siguiente json: 
+              {‘mensaje’:‘hola mundo’}
+*/
+
+app.get('/api/', (req, res) => {
+  res.status(200).json({ mensaje: "hola mundo" });
+});
+            
+/*
+2.- Agrega un endpoint ‘/api/suma’ que responda a una 
+petición de tipo GET con la suma de dos números que 
+reciba mediante las querys num1 y num2. El servidor
+debe responder con un código de estado 200 y un json 
+
+como el siguiente:
+                {‘resultado’: 7}
+
+
+3.- Agrega un endpoint ‘/api/usuario/’ que responda a
+una petición de tipo GET con el nombre que sea 
+recibido a través de params. El servidor debe responder
+con un código de estado 200 y un json como este:
+              {‘usuario’: ‘Edwin’}
+
+4.- Agrega un endpoint ‘/api/swapi’ que responda a una
+petición de tipo GET con el personaje solicitado de 
+                https://swapi.co/
+El cliente debe mandar el número de personaje mediante
+params. La respuesta del servidor debe lucir algo así
+            {‘personaje’: {
+                ‘name’: ‘Luke Skywalker’,
+                ...,
+            }}
+
+5.- Agrega un endpoint ’/api/body que responda a una
+petición de tipo PUT con el body que el cliente envíe al hacer la petición. 
+
+Ejemplo: cliente envía un body desde postman o insomnia que luce como este:
+
+            { “nombre”: “Maui”, “ocupacion”: “Sensei” }
+*/
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
