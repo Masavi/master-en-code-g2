335b6c763c360687c1b692b2b6ea2d9c01b5a36d
maui <maui@devf.mx>
2021-03-23 20:28:39 -0600

    Agregar solucion ejercicio 2

--- 7_Kata_Backend/01_mi_primera_api/ejercicios.js
+++ 7_Kata_Backend/01_mi_primera_api/ejercicios.js
@@ -22,8 +22,17 @@ debe responder con un código de estado 200 y un json
 
 como el siguiente:
                 {‘resultado’: 7}
+*/
 
+app.get('/api/suma', (req, res) => {
+  const { num1, num2 } = req.query;
 
+  res.status(200).json({
+    message: parseInt(num1) + parseInt(num2)
+  });
+});
+
+/*
 3.- Agrega un endpoint ‘/api/usuario/’ que responda a
 una petición de tipo GET con el nombre que sea 
 recibido a través de params. El servidor debe responder
