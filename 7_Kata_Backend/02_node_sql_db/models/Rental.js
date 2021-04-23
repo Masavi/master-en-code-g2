50c6ad0a0daa84373e100832ec4608938de53502
maui <maui@devf.mx>
2021-04-22 21:25:59 -0500

    refactor rentals description -> details

--- 7_Kata_Backend/02_node_sql_db/models/Rental.js
+++ 7_Kata_Backend/02_node_sql_db/models/Rental.js
@@ -2,7 +2,7 @@ const knex = require('../config');
 const createKnexModel = require('../utils/createKnexModel');
 
 const TABLE = 'rentals';
-const TABLE_COLUMNS = ['rental_id', 'title', 'address', 'guests', 'description', 'is_active', 'created_at'];
+const TABLE_COLUMNS = ['rental_id', 'title', 'address', 'guests', 'details', 'is_active', 'created_at'];
 const TABLE_ID = 'rental_id';
 
 const Rental = createKnexModel(knex, TABLE, TABLE_COLUMNS, TABLE_ID);
