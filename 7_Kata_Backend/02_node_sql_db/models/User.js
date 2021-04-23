aecee8dcbb6865806cee2a8cd3db018d637d120e
maui <maui@devf.mx>
2021-04-22 20:38:42 -0500

    Generar utils function para crear modelos genericos y refactorizar el modelo user

--- 7_Kata_Backend/02_node_sql_db/models/User.js
+++ 7_Kata_Backend/02_node_sql_db/models/User.js
@@ -1,41 +1,19 @@
 const knex = require('../config');
+const createKnexModel = require('../utils/createKnexModel');
 
-const create = (bodyUser) => {
-  return knex('users').insert(bodyUser);
-}
+const TABLE = 'users';
+const TABLE_COLUMNS = ['user_id', 'first_name', 'last_name', 'email', 'phone', 'biography', 'is_active', 'created_at'];
+const TABLE_ID = 'user_id';
 
-const findAll = () => {
-  return knex
-    .select(['user_id', 'first_name', 'last_name', 'email', 'phone', 'biography', 'is_active', 'created_at'])
-    .from('users');
-}
+const User = createKnexModel(knex, TABLE, TABLE_COLUMNS, TABLE_ID);
 
-const findOneById = (id) => {
-  return knex
-    .select(['user_id', 'first_name', 'last_name', 'email', 'phone', 'biography', 'is_active', 'created_at'])
-    .from('users')
-    .where({ user_id: id, is_active: true });
-}
+// Esto es lo que debemos exportar al final
+// const User = {
+//   create,
+//   findAll,
+//   findOneById,
+//   updateOneById,
+//   deleteOneById,
+// }
 
-const updateOneById = (id, updateBody) => {
-  return knex
-    .update(updateBody)
-    .from('users')
-    .where({ user_id: id });
-}
-
-// Borrado Lógico (desactivar)
-const deleteOneById = (id) => {
-  return knex
-    .update({ is_active: false })
-    .from('users')
-    .where({ user_id: id });
-}
-
-module.exports = {
-  create,
-  findAll,
-  findOneById,
-  updateOneById,
-  deleteOneById,
-}
+module.exports = User;
---
+++ 7_Kata_Backend/02_node_sql_db/utils/createKnexModel.js
@@ -0,0 +1,44 @@
+function createKnexModel(knex, tableName, tableColumns, tableId) {
+  const create = (body) => {
+    return knex(tableName).insert(body);
+  }
+  
+  const findAll = () => {
+    return knex
+      .select(tableColumns)
+      .from(tableName);
+  }
+  
+  const findOneById = (id) => {
+    return knex
+      .select(tableColumns)
+      .from(tableName)
+      .where({ [tableId]: id, is_active: true });
+  }
+  
+  const updateOneById = (id, updateBody) => {
+    return knex
+      .update(updateBody)
+      .from(tableName)
+      .where({ [tableId]: id });
+  }
+  
+  // Borrado Lógico (desactivar)
+  const deleteOneById = (id) => {
+    return knex
+      .update({ is_active: false })
+      .from(tableName)
+      .where({ [tableId]: id });
+  }
+
+  return {
+    create,
+    findAll,
+    findOneById,
+    updateOneById,
+    deleteOneById,
+  }
+
+}
+
+module.exports = createKnexModel;
