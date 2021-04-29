e88a5588ca48742386dbbb2b99ba1ad55dbca9fe
maui <maui@devf.mx>
2021-04-28 20:03:20 -0500

    Agregar middleware de verificacion de token

--- 7_Kata_Backend/02_node_sql_db/index.js
+++ 7_Kata_Backend/02_node_sql_db/index.js
@@ -5,18 +5,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use((req,res,next) => { haga cosas y al final ejecute next})
 /*
   Podemos utilizar middlewares personalizados que afecten a todos los endpoints
 */
-// const { showTime } = require('./middlewares');
-// app.use(showTime);
 
 /*
   Podemos utilizar middlewares directamente en un endpoint
 */
-// app.get('/', showTime, (req, res) => res.status(200).json({ message: 'Hello World' }));
-app.get('/', (req, res) => res.status(200).json({ message: 'Hello World' }));
+
+const { verifyToken } = require('./middlewares');
+
+app.get('/', verifyToken, (req, res) => res.status(200).json({ message: 'Hello World' }));
 
 app.use('/api/v1', require('./routers'));
 
--- 7_Kata_Backend/02_node_sql_db/middlewares/index.js
+++ 7_Kata_Backend/02_node_sql_db/middlewares/index.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'mipasswordultrachido';
+
 module.exports = {
   /* 
     Un middleware es aquella función
@@ -13,4 +16,17 @@ module.exports = {
     console.log(req.method, req.url);
     next();
   },
+  verifyToken: async (req, res, next) => {
+    const { authorization } = req.headers;
+    // 'Bearer header.payload.token' -> ['Bearer', 'header.payload.token'] -> [1] -> 'header...'   
+    try {
+      const token = authorization.split(' ')[1];
+      const decoded = jwt.verify(token, JWT_SECRET);
+      req.user = decoded;
+      next(); 
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json({ error: "unauthorized, you must provide Authorization as 'Bearer token'" });
+    }
+  }
 } 
