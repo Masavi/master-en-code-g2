cc205662e064d5eb93c75246b3e34d7bf1370b36
maui <maui@devf.mx>
2021-04-29 19:38:05 -0500

    Agregar columna 'role' a User Model

--- 7_Kata_Backend/02_node_sql_db/models/User.js
+++ 7_Kata_Backend/02_node_sql_db/models/User.js
@@ -2,7 +2,17 @@ const knex = require('../config');
 const createKnexModel = require('../utils/createKnexModel');
 
 const TABLE = 'users';
-const TABLE_COLUMNS = ['user_id', 'first_name', 'last_name', 'email', 'phone', 'biography', 'is_active', 'created_at'];
+const TABLE_COLUMNS = [
+  'user_id',
+  'first_name',
+  'last_name',
+  'email',
+  'phone',
+  'biography',
+  'is_active',
+  'created_at',
+  'role',
+];
 const TABLE_ID = 'user_id';
 
 const User = createKnexModel(knex, TABLE, TABLE_COLUMNS, TABLE_ID);
