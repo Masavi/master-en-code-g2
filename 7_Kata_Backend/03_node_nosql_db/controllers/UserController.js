ecf5538331d67546f65d31c5b2d7b94b9b1e7fff
maui <maui@devf.mx>
2021-05-18 20:27:13 -0500

    Agregar modelo, encargado de comunicarse con la base de datos

--- 7_Kata_Backend/03_node_nosql_db/controllers/UserController.js
+++ 7_Kata_Backend/03_node_nosql_db/controllers/UserController.js
@@ -1,5 +1,8 @@
+const User = require('../models/User');
+
 module.exports = {
   create: (req, res) => {
+    // User.create(req.body)
     res.status(201).json({ message: 'user created' });
   },
   findAll: (req, res) => {
