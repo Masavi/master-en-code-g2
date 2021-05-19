21e03a3265cb8258d5d6e292b9134d96793b16d1
maui <maui@devf.mx>
2021-05-18 21:25:19 -0500

    Agregar conexion a mongo atlas mediante mongoose y variable de entorno

--- 7_Kata_Backend/03_node_nosql_db/index.js
+++ 7_Kata_Backend/03_node_nosql_db/index.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express(); // Aplicación de API
 const PORT = 4020 || process.env.PORT;
+
+mongoose.connect(process.env.MONGO_ATLAS_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('Database connected'))
+  .catch(() => console.log('Error connecting to database...'));
 
 app.get('/prueba', (req, res) => {
   res.status(200).json({ message: 'Todo cool' });
