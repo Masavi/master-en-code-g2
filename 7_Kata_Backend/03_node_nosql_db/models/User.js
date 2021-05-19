1b65e65e0f0d0ed1f3cf9b306aab7c1931dfa49e
maui <maui@devf.mx>
2021-05-18 21:44:20 -0500

    Agregar modelo de Usuario con schema de mongoose

--- 7_Kata_Backend/03_node_nosql_db/models/User.js
+++ 7_Kata_Backend/03_node_nosql_db/models/User.js
@@ -1,3 +1,36 @@
-const User = {};
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: 'GUEST',
+    enum: ['GUEST', 'ADMIN'],
+  },
+  profile_pic: {
+    type: String,
+    default: 'https://ca.slack-edge.com/TE1NJDVGU-UFLF10755-78d87551fb96-512'
+  },
+})
+
+/**
+ * El modelo es el objeto que nos permite interactuar
+ * con una colección dada.
+ * 
+ * En este caso, el modelo 'User' nos permitirá interactuar
+ * con la colección 'Users'.
+ */ 
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
