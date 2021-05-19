1eb538472976017be2072b86d847062c622edc46
maui <maui@devf.mx>
2021-05-18 21:58:15 -0500

    Agregar creacion de users mediante el modelo User

--- 7_Kata_Backend/03_node_nosql_db/controllers/UserController.js
+++ 7_Kata_Backend/03_node_nosql_db/controllers/UserController.js
@@ -1,9 +1,20 @@
 const User = require('../models/User');
 
 module.exports = {
-  create: (req, res) => {
-    // User.create(req.body)
-    res.status(201).json({ message: 'user created' });
+  create: async (req, res) => {
+    /*
+      Opción 1: Generar una instancia con "new"
+    */
+    // const newUser = new User(req.body);
+    // const response = await newUser.save();
+
+    try {
+      console.log(req.body)
+      const newUser = await User.create(req.body);
+      res.status(201).json({ message: 'user created', user: newUser }); 
+    } catch (error) {
+      res.status(400).json({ message: 'error creating user', error });
+    }
   },
   findAll: (req, res) => {
     res.status(200).json({ message: 'users list obtained', users: [] });
