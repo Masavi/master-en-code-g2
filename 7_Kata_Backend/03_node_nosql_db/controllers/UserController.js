7fff767ac719f629e9be9b9c725136fbbf9a0897
maui <maui@devf.mx>
2021-05-19 20:18:15 -0500

    Agregar endpoint y logica para encontrar usuario por id

--- 7_Kata_Backend/03_node_nosql_db/controllers/UserController.js
+++ 7_Kata_Backend/03_node_nosql_db/controllers/UserController.js
@@ -24,4 +24,14 @@ module.exports = {
       res.status(400).json({ message: 'error fetching users', error });
     }
   },
+  findOne: async (req, res) => {
+    // const { idUser } = req.params;
+    const id = req.params.idUser;
+    try {
+      const user = await User.findById(id);
+      return res.status(200).json({ message: 'user found', user }) 
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
+  },
 }
--- 7_Kata_Backend/03_node_nosql_db/routers/UserRouter.js
+++ 7_Kata_Backend/03_node_nosql_db/routers/UserRouter.js
@@ -2,8 +2,19 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 
+// Create
 router.post('/users', UserController.create);
 
+// Read (All)
 router.get('/users', UserController.findAll);
+
+// Read (One)
+router.get('/users/:idUser', UserController.findOne);
+
+// Upddate (One)
+
+// Delete (Logical, One)
+
+// Delete (Physical, One)
 
 module.exports = router;
