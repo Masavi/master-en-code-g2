c470dd980464a73dec23f66d62af3f29b4b9f7a4
maui <maui@devf.mx>
2021-05-25 20:12:38 -0500

    Agregar schema de Post para validar posts embebidos en User

---
+++ 7_Kata_Backend/03_node_nosql_db/models/Post.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  image: {
+    type: String,
+    default: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fspectrumconsultants.com%2Fwp-content%2Fuploads%2F2020%2F05%2Fstock-photo-typewriter-header-704430556.png&f=1&nofb=1',
+  }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = { PostSchema, Post };
