d3aa769b3abc50efa56bf0a316bbb8342f86bba8
maui <maui@devf.mx>
2021-05-27 20:35:23 -0500

    Agregar archivo de configuracion para Bucket de Firebase, con funcion para subir archivos

---
+++ 7_Kata_Backend/03_node_nosql_db/utils/storage.js
@@ -0,0 +1,48 @@
+const { Storage } = require('@google-cloud/storage');
+
+const storage = Storage({
+  projectId: "", // Desde Firebase Settings
+  keyFilename: "service.json",
+});
+
+// Mucho ojo: la url NO LLEVA gs:// al inicio
+const bucket = storage.bucket("");
+
+module.exports = (file) =>{
+
+  return new Promise((resolve,reject) => {
+      if(!file) reject("No hay ningun arhivo");
+
+      const newFilename = `${file.originalname}_${Date.now()}`; // esto va a renombra el archivo
+
+      const fileUpload  =  bucket.file(newFilename); // voy a crear un nuevo archivo
+
+      /*
+        Opcional: cómo podrías validar tipos de archivo
+      */
+      /*
+        const valid_mimetypes = ['image/jpeg', 'image/png']
+        if(valid_mimetypes.indexOf(file.mimetype) === -1) reject('Es necesario enviar un tipo valido')
+      */
+
+      const blobStream =  fileUpload.createWriteStream({ //stream de datos le voy a mandar los pedicitos de mi archivo
+          metadata: {
+              contentType: file.mimetype  // que tipo de archivo es el que te voy a mandar
+          }
+      })
+
+      blobStream.on('error', (error) => {
+          reject(error)
+      }) // si pasa un error la promesa debe regresar un error
+
+  
+      blobStream.on('finish',() =>{
+          const url = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${fileUpload.name}?alt=media`
+          resolve(url)
+
+      }) // si todo sale bien regresame la url de mi archivo
+
+      blobStream.end(file.buffer); // aqui empiezo la transmision de datos del backend al bucket
+  })
+
+}
