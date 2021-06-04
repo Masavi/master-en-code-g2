ba969c1676745c6c084582b2ca73d65430d352f2
maui <maui@devf.nx>
2021-06-03 21:41:06 -0500

    Agregar tests de search dentro de home

--- 5_Kata_Frontend/my-first-react-test/src/Home.spec.js
+++ 5_Kata_Frontend/my-first-react-test/src/Home.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Home from './Home';
+import Search from './Search';
 
 describe('Home Component', () => {
     it('renders home component', () => {
@@ -8,6 +9,44 @@ describe('Home Component', () => {
             <Home />
         );
         // screen.debug();
+    });
+
+    it('renders home with search component inside', () => {
+        render(<Home />);
+        const label = screen.getByText('Mi búsqueda');
+        expect(label).toBeInTheDocument();
+    });
+
+    it('tests input text on search', () => {
+        render(<Home />);
+        const input = screen.getByTestId('idTestSearch');
+        fireEvent.change(input, {
+            target: {
+                value: 'una busqueda mas',
+            }
+        })
+        screen.debug();
+    });
+
+    it('detects that onChange executes properly', () => {
+        const onChangeFunction = jest.fn();
+
+        render(
+            <Search
+                value=""
+                onChange={onChangeFunction}
+                titulo="Mi barra de busqueda"
+            />
+        )
+
+        const input = screen.getByTestId('idTestSearch');
+        fireEvent.change(input, {
+            target: {
+                value: 'cualquier cosa',
+            }
+        })
+
+        expect(onChangeFunction).toHaveBeenCalledTimes(1);
     });
 
 });
