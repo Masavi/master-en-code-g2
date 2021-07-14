ab22764e64bd6bc6fe3055991934ab2292535969
maui <maui@devf.mx>
2021-07-13 20:30:47 -0500

    feat: install dotenv and connect to mongo atlas db through env variable

--- 8_Kata_Data_Science/01_Importar_datos/importDataSet.js
+++ 8_Kata_Data_Science/01_Importar_datos/importDataSet.js
@@ -1,14 +1,23 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('Database connected'))
+  .catch(() => console.log('Error connecting to database...'));
 
 fs.createReadStream('e.csv')
   .pipe(csv())
   .on('data', (data) => results.push(data))
   .on('end', () => {
     // results.slice(0, 100).map(data => console.log(data))
-    for (let index = 0; index < 100; index++) {
-      console.log(results[index]);
-    }
+    // for (let index = 0; index < 100; index++) {
+    //   console.log(results[index]);
+    // }
   });
 
