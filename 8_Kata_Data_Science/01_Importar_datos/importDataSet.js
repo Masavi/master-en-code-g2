bf1ed60d1da92823851b0654e17b9d6453430882
maui <maui@devf.mx>
2021-07-13 21:25:22 -0500

    feat: clean and structure rows from csv to import into database

--- 8_Kata_Data_Science/01_Importar_datos/importDataSet.js
+++ 8_Kata_Data_Science/01_Importar_datos/importDataSet.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const csv = require('csv-parser')
 const fs = require('fs')
-const results = [];
 
 mongoose.connect(
     process.env.MONGODB_URI,
@@ -11,13 +10,48 @@ mongoose.connect(
   .then(() => console.log('Database connected'))
   .catch(() => console.log('Error connecting to database...'));
 
+const results = [];  
+
 fs.createReadStream('e.csv')
   .pipe(csv())
-  .on('data', (data) => results.push(data))
+  .on('data', (data) => {
+    const {
+      device_mac,
+      branch_office,
+      month_tz,
+      day_tz,
+      day_of_week_tz,
+      hour_tz,
+      visitor,
+      tiempodeses,
+    } = data;
+
+    const day = parseInt(day_tz);
+    const dayString = day > 9 ? `${day}` : `0${day}`;
+        
+    const hour = parseInt(hour_tz)
+    const hourString = hour > 9 ? `${hour}` : `0${hour}`;
+
+    const newClientSession = {
+      device_mac,
+      branch_office: parseInt(branch_office),
+      month_tz: parseInt(month_tz),
+      day_tz: day,
+      day_of_week_tz,
+      hour_tz: hour,
+      is_visitor: visitor === 'true',
+      date_tz: new Date(`2016-${month_tz}-${dayString}T${hourString}:00:00Z`),
+      // truthy - 1, [], 'acasd', true
+      // falsy - 0, null, undefined, ''
+      session_duration: parseInt(tiempodeses),
+    }
+
+    results.push(newClientSession);
+  })
   .on('end', () => {
     // results.slice(0, 100).map(data => console.log(data))
-    // for (let index = 0; index < 100; index++) {
-    //   console.log(results[index]);
-    // }
+    for (let index = 0; index < 5; index++) {
+      console.log(results[index]);
+    }
   });
 
