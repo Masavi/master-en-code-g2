97d57cf1fa13183518833d5793a408cb1d268156
maui <maui@devf.mx>
2021-07-13 20:17:49 -0500

    feat: add client session model and schema

---
+++ 8_Kata_Data_Science/01_Importar_datos/ClientSession.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const clientSessionSchema = new mongoose.Schema({
+  device_mac: {
+    type: String,
+    required: true,
+  },
+  branch_office: {
+    type: Number,
+    required: true,
+  },
+  month_tz: {
+    type: Number,
+    required: true,
+  },
+  day_tz: {
+    type: Number,
+    required: true,
+  },
+  day_of_week_tz: {
+    type: String,
+    required: true,
+  },
+  hour_tz: {
+    type: Number,
+    required: true,
+  },
+  date_tz: {
+    type: Date,
+  },
+  is_visitor: {
+    type: Boolean,
+    required: true,
+  },
+  session_duration: {
+    type: Number,
+    required: true,
+  },
+});
+
+const ClientSession = mongoose.model('Session', clientSessionSchema);
+
+module.exports = ClientSession;
